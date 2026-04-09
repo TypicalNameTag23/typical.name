a4563875313f0ec4d72ac160ccbd509c932a8f9b
TypicalNameTag23 <typicalnametag23@typical.name>
2026-04-09 01:45:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const endpoint = "https://pokeapi.co/api/v2"
-const betterEndpoint = "https://titanic-survival-predictor-zfob.onrender.com/predict"
+const betterEndpoint = "https://titanic-survival-predictor-fkrl.onrender.com/predict"
 
 const searchBox = document.getElementById("search-box");
 const errorDisplay = document.getElementById("error-display");
@@ -7,50 +7,46 @@ const teamDisplay = document.getElementById("team-display");
 document.getElementById("addButton")
     .addEventListener("click", async function (e) {
         e.preventDefault();
-
-        if (teamDisplay.children.length >= 6) {
-            errorDisplay.textContent = "Hey bub thats a bit too many pokemon get a grip";
-            return;
-        }
+        //
 
         // const pokemon = await getPokemonAsync(searchBox.value);
         const thingsInSearchBoxOrSomething = searchBox.value.split(',')
         const maybeASuccessfulResponseIdk = await getSurvivalChanceAsync(thingsInSearchBoxOrSomething)
         console.log(maybeASuccessfulResponseIdk);
-        
+        displaySurvivalChance(maybeASuccessfulResponseIdk)
         // addPokemonCard(pokemon)
 })
 
 function displaySurvivalChance(successResponseMaybeIWontCheck) {
+    const card = document.createElement("div");
     card.innerHTML = `
         <div>
-            <p>${successResponseMaybeIWontCheck}
+            <p>Survival Chance: ${successResponseMaybeIWontCheck.predicted_survival_chance}</p>
+            <p>${successResponseMaybeIWontCheck.prediction == 1 ? "Survived" : "AHAHAJ LOSER YOUR DEAD HAGAHHSADHSA H"}</p>
+        </div>
     `
+    teamDisplay.appendChild(card)
 }
 
 async function getSurvivalChanceAsync(theThings) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    const requestBody = {
+        'Pclass': theThings[0],
+        'Sex': theThings[1],
+        'Age': theThings[2],
+        'SibSp': theThings[3],
+        'Parch': theThings[4],
+        'Fare': theThings[5],
+        'Embarked': theThings[6]
+    }
+    console.log(JSON.stringify(requestBody));
 
-    const raw = JSON.stringify({
-        "Pclass": "3",
-        "Sex": " female",
-        "Age": " 54",
-        "SibSp": " 1",
-        "Parch": " 0",
-        "Fare": " 70.25",
-        "Embarked": " Q"
-    });
-
-    const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-    };
-
-    const resp = await fetch("https://titanic-survival-predictor-fkrl.onrender.com/predict", requestOptions);
-    console.log(resp)
+    return await (await fetch(betterEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })).json();
 }
 
 document.getElementById("clearButton")
